fec0696fe7e9c2c4ea07ca5ec7942cd2a5b8071c
Sean Stavropoulos <sean.stavro@gmail.com>
2014-02-03 09:51:18 -0800

    Change cookie name

--- web_server/config/initializers/session_store.rb
+++ web_server/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-cookie_key = "_snakesnack_session"
+cookie_key = "_snakesnack_session_web"
 
 cookie_store_options = {
   key: cookie_key
