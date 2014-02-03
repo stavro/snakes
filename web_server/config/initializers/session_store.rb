7f8b33f117ef7ff48a16f6528068e94be8cbc269
Sean Stavropoulos <sean.stavro@gmail.com>
2014-02-02 19:44:22 -0800

    remove cross domain cookies

--- web_server/config/initializers/session_store.rb
+++ web_server/config/initializers/session_store.rb
@@ -1,18 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-domain_map = {
-  'development' => '.snakesnack.dev',
-  'production' => '.herokuapp.com',
-  'staging'    => '.herokuapp.com'
-}
-
 cookie_key = "_snakesnack_session"
 
-custom_domain = domain_map[Rails.env].presence || nil
-
 cookie_store_options = {
-  key: cookie_key,
-  domain: custom_domain,
+  key: cookie_key
 }
 
 SnakeSnack::Application.config.session_store :cookie_store, cookie_store_options
